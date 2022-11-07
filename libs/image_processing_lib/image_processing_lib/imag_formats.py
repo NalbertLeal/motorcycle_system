15eba44f0e740e45a338974c38218a852cd02fd3
Nalbert Gabriel Melo Leal <nalbertgml@gmail.com>
2022-11-07 19:23:04 -0300

    Feat: Funções que permitem a compressão de matrizes de imagens para os formatos jpg ou png

--- libs/image_processing_lib/image_processing_lib/imag_formats.py
+++ libs/image_processing_lib/image_processing_lib/imag_formats.py
@@ -1,21 +1,33 @@
 import cv2
+import io
 import numpy as np
 from typing import Tuple
 
 YOLOv5_IMAGE_HEIGHT = 640
 YOLOv5_IMAGE_WIDTH = 640
 
-def matrix_to_png(
-    img: np.ndarray,
-    size: Tuple=(YOLOv5_IMAGE_HEIGHT, YOLOv5_IMAGE_WIDTH),
-) -> bytes:
-    ...
+def _img_compress(format: str, frame: np.ndarray) -> bytes:
+    format_extension = f'.{format}'
+    is_success, buffer = cv2.imencode(format_extension, frame)
+    if not is_success:
+        raise BaseException('Image compression with a problem')
+    io_buf = io.BytesIO(buffer)
+    io_buf.seek(0)
+    content = io_buf.read()
+    io_buf.close()
+    return content
 
-def matrix_to_jpg(
-    img: np.ndarray,
-    size: Tuple=(YOLOv5_IMAGE_HEIGHT, YOLOv5_IMAGE_WIDTH),
-) -> bytes:
-    ...
+def matrix_to_png(img: np.ndarray) -> bytes:
+    _img_compress('png', img)
+
+# def matrix_to_jpg(
+#     img: np.ndarray,
+#     size: Tuple=(YOLOv5_IMAGE_HEIGHT, YOLOv5_IMAGE_WIDTH),
+# ) -> bytes:
+#     ...
+
+def matrix_to_jpg(img: np.ndarray) -> bytes:
+    _img_compress('jpg', img)
 
 def image_to_matrix(
     img: bytes,
