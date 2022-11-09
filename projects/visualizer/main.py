b6b44a54792d37a40095a3f9a7d03645ea5a0822
Nalbert Gabriel Melo Leal <nalbertgml@gmail.com>
2022-11-08 22:38:00 -0300

    Feat: Visualizador agora suporta visualização das placas encontradas

--- projects/visualizer/main.py
+++ projects/visualizer/main.py
@@ -11,7 +11,7 @@ from src import consumer as src_consumer
 
 KAFKA_HOST = os.environ.get('KAFKA_HOST', default='localhost:9092')
 # KAFKA_CONSUMER_TOPIC = os.environ.get('KAFKA_CONSUMER_TOPIC', default='motorcycles')
-KAFKA_CONSUMER_TOPIC = os.environ.get('KAFKA_CONSUMER_TOPIC', default='plates')
+KAFKA_CONSUMER_TOPIC = os.environ.get('KAFKA_CONSUMER_TOPIC', default='plates_text')
 KAFKA_CONSUMER_PARTITIONS = int(os.environ.get('KAFKA_CONSUMER_PARTITIONS', default='1'))
 
 consumer = kafka_lib_consumer.create_consumer(
--- projects/visualizer/src/consumer.py
+++ projects/visualizer/src/consumer.py
@@ -9,7 +9,7 @@ from kafka_lib import producer as kafka_lib_producer
 import frames_pb2 as frames_pb
 
 def _deserialize_image(data) -> Tuple[bytes, str, int]:
-    message = frames_pb.Plate()
+    message = frames_pb.PlateText()
     message.ParseFromString(data)
 
     matrix_img = image_formats.png_to_matrix(message.frame.frame)
@@ -17,7 +17,20 @@ def _deserialize_image(data) -> Tuple[bytes, str, int]:
         .reshape(message.frame.shape)
     bbox = np.frombuffer(message.bbox.box, dtype=np.int64)\
         .reshape(message.bbox.shape)
-    return bbox, message.bbox.label, message.frame.frame_number, frame
+    print(message.bbox.label)
+    return bbox, message.frame.frame_number, frame, message.bbox.plate_text
+
+# # Plate Visualizer
+# def _deserialize_image(data) -> Tuple[bytes, str, int]:
+#     message = frames_pb.Plate()
+#     message.ParseFromString(data)
+
+#     matrix_img = image_formats.png_to_matrix(message.frame.frame)
+#     frame = np.frombuffer(matrix_img, dtype=np.uint8)\
+#         .reshape(message.frame.shape)
+#     bbox = np.frombuffer(message.bbox.box, dtype=np.int64)\
+#         .reshape(message.bbox.shape)
+#     return bbox, message.bbox.label, message.frame.frame_number, frame
 
 # def _deserialize_image(data) -> Tuple[bytes, str, int]:
 #     message = frames_pb.Plate()
@@ -32,12 +45,14 @@ def _deserialize_image(data) -> Tuple[bytes, str, int]:
 im_path = '/home/nalbertgml/Documentos/motor-plates-detection-monorepo/projects/visualizer/frames/frame_'
 def consume_frames(data):
     value = data.value
-    bbox, label, frame_number, frame = _deserialize_image(value)
+    bbox, frame_number, frame, plate_text  = _deserialize_image(value) # plate text
+    # bbox, label, frame_number, frame,  = _deserialize_image(value) # plate
     # bbox, frame_number, frame = _deserialize_image(value)
     
     resized_frame = image_matrix.resizeAndPad(frame)
     [x, y, w, h] = bbox
     cv2.rectangle(resized_frame, (x, y), (w, h), (0, 255, 0), 2)
-    cv2.imshow(label, resized_frame)
+    cv2.imshow(plate_text, resized_frame) # plate text
+    # cv2.imshow(label, resized_frame) # plate
     # cv2.imshow('frame', resized_frame)
     cv2.waitKey(1000)
