7a4ae677829104ac1efdfe8514bf07172b37b80a
Nalbert Gabriel Melo Leal <nalbertgml@gmail.com>
2022-11-08 22:39:55 -0300

    Fix: Serviço de caracteres suportando a nova estrutura da mensagem PlateText do protobuf

--- projects/plate_characters_detector/src/kafka/consumer.py
+++ projects/plate_characters_detector/src/kafka/consumer.py
@@ -1,4 +1,4 @@
-from os import environ
+import os
 from typing import Tuple
 
 import numpy as np
@@ -6,9 +6,46 @@ import cv2
 
 from image_processing_lib import image_formats
 from kafka_lib import consumer as lib_consumer
+from kafka_lib import producer as kafka_lib_producer
 import frames_pb2 as frames_pb
 from src.nn.recognition import CharactersRecognition
 from src.nn.segmentation import CharactersSegmentation
+
+KAFKA_HOST = os.environ.get('KAFKA_HOST', default='localhost:9092')
+KAFKA_PRODUCER_TOPIC = os.environ.get('KAFKA_PRODUCER_TOPIC', default='plates_text')
+KAFKA_PRODUCER_PARTITIONS = int(os.environ.get('KAFKA_PRODUCER_PARTITIONS', default=1))
+MONGO_HOST = os.environ.get('MONGO_HOST', default='localhost')
+MONGO_PORT = os.environ.get('MONGO_PORT', default='27017')
+MONGO_USER = os.environ.get('MONGO_USER', default='root')
+MONGO_PASS = os.environ.get('MONGO_PASS', default='231564')
+
+kafka_producer = kafka_lib_producer.create_producer(
+    [KAFKA_HOST],
+    [KAFKA_PRODUCER_TOPIC],
+    KAFKA_PRODUCER_PARTITIONS
+)
+
+def _serialize_image(data) -> bytes:
+    processing_id, plate_id, box, label, plate_content, frame, frame_number = data
+    message = frames_pb.PlateText()
+    message.processing_id = str(processing_id)
+    message.plate_id = str(plate_id)
+
+    message.frame.frame_number = frame_number
+    message.frame.shape.extend(frame.shape)
+    png_img = image_formats.matrix_to_png(frame)
+    message.frame.frame = png_img
+
+    message.bbox.shape.extend(box.shape)
+    message.bbox.box = box.tobytes()
+    message.bbox.label = label
+    message.bbox.plate_text = plate_content
+
+    return message.SerializeToString()
+
+def _send_frame(producer, data):
+    message = _serialize_image(data)
+    kafka_lib_producer.send_to_producer(producer, message)
 
 def deserialize(data: bytes) -> Tuple:
     message = frames_pb.Plate()
@@ -60,16 +97,13 @@ def receive_image(data: bytes):
     
     plate_1, positions1, seg_conf1, plate_2, positions2, seg_conf2 = plate_img_segmentation(motorcycle_frame)
     
-    chars_positions = positions1 + positions2
-    #print(label)
     [plate_1_text, _] = chars_recognition(plate_1, label, positions1, True)
-    #print('ok1')
     [plate_2_text, _] = chars_recognition(plate_2, label, positions2, False)
-    #print('ok2')
-    #print('plate_1_text = ', plate_1_text)
-    #print('plate_2_text = ', plate_2_text)
 
-    text = plate_1_text + plate_2_text
-    print(text)
+    plate_content = plate_1_text + '-' + plate_2_text
+    print(plate_content)
+
+    data = processing_id, plate_id, bbox, label, plate_content, frame, frame_number
+    _send_frame(kafka_producer, data)
 
     # send recognition result to database
