5927afb4391346fcdb2340ef025c359a5aa2e7bf
Nalbert Gabriel Melo Leal <nalbertgml@gmail.com>
2022-11-19 01:20:28 -0300

    Feat: biblioteca de inferência de modelos do onnx suportando modelos com FP16

--- libs/image_processing_lib/image_processing_lib/onnx.py
+++ libs/image_processing_lib/image_processing_lib/onnx.py
@@ -6,7 +6,7 @@ from .image_matrix import resizeAndPad, resize_image, normalize, add_batch_dimen
 YOLOv5_IMAGE_HEIGHT = 640
 YOLOv5_IMAGE_WIDTH = 640
 
-def preprocess_image_to_onnx(frame: np.ndarray, preserve_aspect_ratio: bool=False) -> np.ndarray:
+def preprocess_image_to_onnx(frame: np.ndarray, preserve_aspect_ratio: bool=False, float_dtype = np.float32) -> np.ndarray:
     '''
         Prepare image to an onnx model process.
     '''
@@ -14,10 +14,10 @@ def preprocess_image_to_onnx(frame: np.ndarray, preserve_aspect_ratio: bool=Fals
         img = resizeAndPad(frame)
     else:
         img = resize_image(frame, YOLOv5_IMAGE_HEIGHT, YOLOv5_IMAGE_WIDTH)
-    img = to_onnx_format(img, YOLOv5_IMAGE_HEIGHT, YOLOv5_IMAGE_WIDTH)
+    img = to_onnx_format(img, YOLOv5_IMAGE_HEIGHT, YOLOv5_IMAGE_WIDTH, float_dtype)
     img = normalize(img)
     img = add_batch_dimension(img)
     return img
 
-def to_onnx_format(img: np.ndarray, height: int=640, width: int=640) -> np.ndarray:
-	return np.ascontiguousarray(img).astype(np.float32).transpose(2, 0, 1)
+def to_onnx_format(img: np.ndarray, height: int=640, width: int=640, float_dtype = np.float32) -> np.ndarray:
+	return np.ascontiguousarray(img).astype(float_dtype).transpose(2, 0, 1)
