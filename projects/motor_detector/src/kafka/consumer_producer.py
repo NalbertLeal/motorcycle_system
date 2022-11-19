4518ece88293751d5b265885f8369e4b29b81155
Nalbert Gabriel Melo Leal <nalbertgml@gmail.com>
2022-11-19 01:19:51 -0300

    Feat: serviço de motor detector suportando onnx com FP16

--- projects/motor_detector/src/kafka/consumer_producer.py
+++ projects/motor_detector/src/kafka/consumer_producer.py
@@ -13,15 +13,23 @@ import frames_pb2 as frames_pb
 
 from src.database import motorcycle
 
+# kafka envs
 KAFKA_HOST = os.environ.get('KAFKA_HOST', default='localhost:9092')
 KAFKA_PRODUCER_TOPIC = os.environ.get('KAFKA_PRODUCER_TOPIC', default='motorcycles')
 KAFKA_PRODUCER_PARTITIONS = int(os.environ.get('KAFKA_PRODUCER_PARTITIONS', default=1))
+# mongo envs
 MONGO_HOST = os.environ.get('MONGO_HOST', default='localhost')
 MONGO_PORT = os.environ.get('MONGO_PORT', default='27017')
 MONGO_USER = os.environ.get('MONGO_USER', default='root')
 MONGO_PASS = os.environ.get('MONGO_PASS', default='231564')
+# model envs
+YOLOv5_IMAGE_HEIGHT = 640
+YOLOv5_IMAGE_WIDTH = 640
+MODEL_PATH = os.environ.get('MODEL_PATH', default='./onnx_weights/yolov5m.onnx')
+CPU_GPU = os.environ.get('CPU_GPU', default='cpu')
+MODEL_IS_FP16 = bool(os.environ.get('MODEL_IS_FP16', default=0))
 
-yolo_v5_onnx_model = model.new_YOLOv5Onnx('./onnx_weights/yolov5m.onnx', 'gpu')
+yolo_v5_onnx_model = model.new_YOLOv5Onnx(MODEL_PATH, CPU_GPU)
 kafka_producer = kafka_lib_producer.create_producer(
     [KAFKA_HOST],
     [KAFKA_PRODUCER_TOPIC],
@@ -78,7 +86,13 @@ def consume_frames(data):
     value = data.value
     processing_id, frame_number, frame = _deserialize_image(value)
 
-    onnx_frame = onnx.preprocess_image_to_onnx(frame, True)
+    model_fp = None
+    if MODEL_IS_FP16:
+        model_fp = np.float16
+    else:
+        model_fp = np.float32
+
+    onnx_frame = onnx.preprocess_image_to_onnx(frame, True, model_fp)
     bboxes = model.run_model(yolo_v5_onnx_model, onnx_frame)
     bboxes = np.array(bboxes)
 
